37d7c103f6fa73fe565fb893e63d11d91e59572e
Chase Davis <chase.davis@gmail.com>
2013-02-08 14:45:45 -0800

    Using MiniBatch K-Means ffor preprocessing of titles

--- copycat/bin/cluster.py
+++ copycat/bin/cluster.py
@@ -1,8 +1,15 @@
-import numpy, heapq, itertools
+import numpy
 import networkx as nx
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.cluster import KMeans, MiniBatchKMeans
 from gensim import similarities, corpora, models
 from gensim.similarities.docsim import Similarity
 from apps.bills.models import Bill
+
+########## CONSTANTS ##########
+
+K_CLUSTERS = 25
 
 ########## CLASSES ##########
 
@@ -18,26 +25,50 @@ class BillCorpus(object):
 ########## MAIN ##########
 
 if __name__ == '__main__':
-    # First obtain documents to test
-    bills = Bill.objects.all()
-    documents = bills.values_list('title', flat=True)
+    # Get the bills and bill text from the database. Also force early evaluation of
+    # the queryset so its contents can be accessed without database queries later.
+    bills = list(Bill.objects.all())
+    billtext = Bill.objects.all().values_list('title', flat=True)
 
-    # Load into gensim dictionary object
-    dictionary = corpora.Dictionary(line.lower().split() for line in documents)
+    # Get word counts for bill titles
+    count_vect = CountVectorizer()
+    X_train_counts = count_vect.fit_transform(billtext)
 
-    # Now create corpus and tfidf model
-    bc = BillCorpus(documents, dictionary)
-    tfidf = models.TfidfModel(bc)
+    # Transform word counts into tf-idf vectors, for feeding into K-Means
+    tf_transformer = TfidfTransformer().fit(X_train_counts)
+    X_train_tf = tf_transformer.transform(X_train_counts)
+
+    # Execute minibatch k-means on the tf-idf vectors. Using minibatch because it runs
+    # orders of magnitude faster than traditional k-means, at the expense of a little precision
+    print '---------- Forming %s initial clusters ----------' % K_CLUSTERS
+    km = MiniBatchKMeans(n_clusters=K_CLUSTERS, init='k-means++', n_init=1,
+        compute_labels=True)
+    km_results = km.fit(X_train_tf)
 
     G = nx.Graph() # Prep the networkX graph for later
 
-    # Create and iterate over the similarity matrix
-    index = Similarity(corpus=tfidf[bc], num_features=tfidf.num_nnz, output_prefix="shard")
-    for i in enumerate(index):
-        # Get the ids of values in each row that have cosine similarity > 0.7
-        for j in numpy.nonzero(i[1] > 0.7)[0]:
-            b1, b2 = bills[i[0]], bills[j]
-            if b1.state <> b2.state:
-                G.add_edge(b1.title, b2.title)
+    # Iterate through each of the k-means clusters
+    for i in range(K_CLUSTERS):
+        # Hash containing the title of the bill and its position in the above bills array
+        id_hash = dict((billtext[id], id) for id in numpy.where(km_results.labels_ == i)[0])
 
-    nx.write_graphml(G, 'graph.graphml')
+        print '---------- Processing cluster %s of %s (%s items) ----------' % (i + 1, K_CLUSTERS, len(id_hash))
+
+        # Load titles into gensim dictionary object
+        dictionary = corpora.Dictionary(line.lower().split() for line in id_hash.keys())
+
+        # Now create corpus and tfidf model
+        bc = BillCorpus(id_hash.keys(), dictionary)
+        tfidf = models.TfidfModel(bc)
+
+        # Create and iterate over the similarity matrix
+        index = Similarity(corpus=tfidf[bc], num_features=tfidf.num_nnz, output_prefix="shard")
+        for i in enumerate(index):
+            for j in numpy.nonzero(i[1] > 0.7)[0]:
+                id1, id2 = id_hash[id_hash.keys()[i[0]]], id_hash[id_hash.keys()[j]]
+                a, b = bills[id1], bills[id2]
+                if a.state <> b.state:
+                    G.add_edge(a.title, b.title)
+
+    # Write matches to a graphml file for processing in Gephi
+    nx.write_graphml(G, 'kgraph.graphml')
