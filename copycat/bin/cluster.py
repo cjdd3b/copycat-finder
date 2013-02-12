78bd9f9bf1298e29b3fce03ac24cfa959660f94a
Chase Davis <chase.davis@gmail.com>
2013-02-12 09:54:45 -0800

    Tweaks to clustering workflow to eliminate weird hash collisions

--- copycat/bin/cluster.py
+++ copycat/bin/cluster.py
@@ -9,7 +9,7 @@ from apps.bills.models import Bill
 
 ########## CONSTANTS ##########
 
-K_CLUSTERS = 25
+K_CLUSTERS = 20
 
 ########## CLASSES ##########
 
@@ -27,8 +27,8 @@ class BillCorpus(object):
 if __name__ == '__main__':
     # Get the bills and bill text from the database. Also force early evaluation of
     # the queryset so its contents can be accessed without database queries later.
-    bills = list(Bill.objects.all())
-    billtext = Bill.objects.all().values_list('title', flat=True)
+    bills = list(Bill.objects.all())[:100000]
+    billtext = Bill.objects.all().values_list('title', flat=True)[:100000]
 
     # Get word counts for bill titles
     count_vect = CountVectorizer()
@@ -44,31 +44,33 @@ if __name__ == '__main__':
     km = MiniBatchKMeans(n_clusters=K_CLUSTERS, init='k-means++', n_init=1, compute_labels=True)
     km_results = km.fit(X_train_tf)
 
-    G = nx.Graph() # Prep the networkX graph for later
+    G = nx.Graph() # Prep an empty networkX graph for later
 
     # Iterate through each of the k-means clusters
     for i in range(K_CLUSTERS):
-        # Hash containing the title of the bill and its position in the above bills array
-        id_hash = dict((billtext[id], id) for id in numpy.where(km_results.labels_ == i)[0])
+        # Lists containing bill text and the bill object's position in the above array. This is super hacky
+        # and could definitely stand to be cleaned up.
+        bill_ids = numpy.where(km_results.labels_ == i)[0]
+        clustertext = [billtext[id] for id in bill_ids]
 
-        print '---------- Processing cluster %s of %s (%s items) ----------' % (i + 1, K_CLUSTERS, len(id_hash))
+        print '---------- Processing cluster %s of %s (%s items) ----------' % (i + 1, K_CLUSTERS, len(text))
 
         # Load titles into gensim dictionary object
-        dictionary = corpora.Dictionary(line.lower().split() for line in id_hash.keys())
+        dictionary = corpora.Dictionary(line.lower().split() for line in clustertext)
 
         # Now create corpus and tfidf model
-        bc = BillCorpus(id_hash.keys(), dictionary)
+        bc = BillCorpus(clustertext, dictionary)
         tfidf = models.TfidfModel(bc)
 
         # Create and iterate over the similarity matrix
         index = Similarity(corpus=tfidf[bc], num_features=tfidf.num_nnz, output_prefix="shard")
         for i in enumerate(index):
             for j in numpy.nonzero(i[1] > 0.7)[0]:
-                id1, id2 = id_hash[id_hash.keys()[i[0]]], id_hash[id_hash.keys()[j]]
+                id1, id2 = bill_ids[[i[0]]], bill_ids[j]
                 bill1, bill2 = bills[id1], bills[id2]
                 if bill1.state <> bill2.state:
-                    G.add_node(bill1.title, state=bill1.state.name, session=bill1.session.name, id=bill1.bill_id)
-                    G.add_node(bill2.title, state=bill2.state.name, session=bill2.session.name, id=bill2.bill_id)
+                    G.add_node(bill1.title, state=bill1.state.name, session=bill1.session.name, bill_id=bill1.bill_id)
+                    G.add_node(bill2.title, state=bill2.state.name, session=bill2.session.name, bill_id=bill2.bill_id)
                     G.add_edge(bill1.title, bill2.title)
 
     # Write matches to a graphml file for processing in Gephi
